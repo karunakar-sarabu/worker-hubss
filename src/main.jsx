3a6f7258fd376de22415d5b3a05c88e50fc33ae3
Karunakar Sarabu <karunakarsarabu9@gmail.com>
2026-06-21 16:33:37 -0700

    Added Google Analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-0Z5ENJKM1S");
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
