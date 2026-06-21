c77205ccb6a8392a272595881a541292f2593b42
Karunakar Sarabu <karunakarsarabu9@gmail.com>
2026-06-21 16:56:07 -0700

    Add GA pageview tracking

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,10 @@ import { BrowserRouter } from 'react-router-dom'
 import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-0Z5ENJKM1S");
+ReactGA.send({
+  hitType: "pageview",
+  page: window.location.pathname,
+});
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
